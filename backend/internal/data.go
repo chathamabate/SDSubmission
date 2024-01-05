45e579b80526d663472e45f4e006f965a6ef708a
chathamabate <chathamabate@gmail.com>
2024-01-05 09:25:19 -0500

    Cleanup

--- backend/internal/data.go
+++ backend/internal/data.go
@@ -2,34 +2,11 @@ package internal
 
 import (
 	sql "database/sql"
-	"errors"
 )
 
-func Foo() {
 
-}
+// First we check that table exists.
 
-
-// The user can insert a single map resulting in one row being inserted.
-// Or the user can insert a list of maps resulting in multiple rows being
-// inserted.
-func insert(db *sql.DB, table string, data interface{}) error {
-    switch v := data.(type) {
-
-    // Single Row Case.
-    case map[string]interface{}:
-        break
-
-    // Multiple Row Case.
-    case []map[string]interface{}:
-        break
-
-        // In this case, let's just 
-
-    default:
-        return errors.New("Unknown data format")
-    }
-
-    
+func insert(db *sql.DB, table string, data []map[string]interface{}) error {
     return nil
 }
