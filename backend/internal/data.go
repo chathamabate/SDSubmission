327e7369875ec0ea30a8387f0bd6a6e67a798c3e
chathamabate <chathamabate@gmail.com>
2024-01-05 12:53:52 -0500

    SQLite progress

--- backend/internal/data.go
+++ backend/internal/data.go
@@ -2,71 +2,127 @@ package internal
 
 import (
 	sql "database/sql"
+	"errors"
+	"fmt"
+    "strings"
 )
 
-// This function confirms that the given two slices of strings 
-// contain equal values regardless of order.
-func equalStringCollections(c1 []string, c2 []string) bool {
-    // Count the frequencies of every string in c2
-    c2Freqs := make(map[string]int)
-    for _, v := range c2 {
-        if _, ok := c2Freqs[v]; ok {
-            c2Freqs[v]++
-        } else {
-            c2Freqs[v] = 1
-        }
-    }
+type SDTypeID uint
 
-    // Confirm all elements in c1 are in c2
-    for _, v := range c1 {
-        freq, ok := c2Freqs[v] 
+// Unsigned integer identifiers for each suported type.
+const (
+    IntTypeID       = iota 
+    TextTypeID    = iota     
 
-        if !ok || freq == 0 {
-            return false
-        }
+    // TODO: Consider adding more.
+)
 
-        c2Freqs[v]--
-    }
-
-    // Confirm there are no elements in c2 which
-    // are not in c1
-    for _, freq := range c2Freqs {
-        if freq > 0 {
-            return false
-        }
-    }
-
-    return true
+// Type identifiers -> SQL type names.
+var SDTypeNames = map[SDTypeID]string {
+    IntTypeID:      "INTEGER",
+    TextTypeID:     "TEXT",
 }
 
-// Given a slice of JSON objects, returns a slice containing
-// every unique key name which appears.
-// (These unique keys will be mapped to SQL table columns) 
-func uniqueKeys(objs []map[string]interface{}) []string {
-    m := make(map[string]bool)
+var SDTypeIDs = map[string]SDTypeID{
+    "INTEGER":  IntTypeID,
+    "TEXT":     TextTypeID,
+}
+
+func GetTypeID(t interface{}) (SDTypeID, error) {
+    switch t.(type) {
+    case int:
+        return IntTypeID, nil
+    case string:
+        return TextTypeID, nil
+    }
+
+    return 0, errors.New("Unknown Type")
+}
+
+// Given a slice of JSON objects, returns a map which maps each
+// unique key name found to its corresponding type.
+//
+// Returns an error if an unknown type is found or the same key is mapped
+// to multiple different types.
+func uniqueKeys(objs []map[string]interface{}) (map[string]SDTypeID, error) {
+    m := make(map[string]SDTypeID)
 
     for _, obj := range objs {
-        for key := range obj {
-            if _, ok := m[key]; !ok {
-                m[key] = true
+        for key, val := range obj {
+            actualTID, err := GetTypeID(val)
+
+            if err != nil {
+                return nil, err
+            }
+
+            expectedTID, ok := m[key]
+            
+            if !ok {
+                m[key] = actualTID
+            } else if actualTID != expectedTID {
+                return nil, fmt.Errorf("Type mismatch on field: %s", key)
             }
         }
     }
 
-    // Translate m into a slice of just its keys.
-    keys := make([]string, len(m))
+    return m, nil
+}
+
+func getStructureString(structure map[string]SDTypeID) string {
+    var sb strings.Builder 
 
     i := 0
-    for key := range m {
-        keys[i] = key
+    for columnName, typeID := range structure {
+        sb.WriteString(columnName)
+        sb.WriteString(" ")
+        sb.WriteString(SDTypeNames[typeID])
+        
+        if i < len(structure) - 1 {
+            sb.WriteString(", ")
+        }
+
         i++
     }
 
-    return keys;
+    return sb.String()
 }
 
-func confirmTable(table string, data []map[string]interface{}) ([]string, error) {
-    return nil, nil
+// Return the table structure in the form of a map which maps column
+// names to data types.
+//
+// NOTE: This assumes the given table exists.
+func getTableStructure(db *sql.DB, table string) (map[string]SDTypeID, error) {
+    rows, err := db.Query("PRAGMA table_info(?)", table)
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+
+    structure := make(map[string]SDTypeID)
+
+    var name string
+    var typeName string
+
+    for rows.Next() {
+        err = rows.Scan(&name, &typeName) 
+
+        if err != nil {
+            return nil, err
+        }
+
+        tid, ok := SDTypeIDs[typeName]
+        if !ok {
+            return nil, fmt.Errorf("Unknown type %s.", typeName)
+        }
+
+        structure[name] = tid
+    }
+
+    return structure, nil
+}
+
+func conformTable(table string, reqColumns map[string]SDTypeID) error {
+    return nil
 }
 
 func insert(db *sql.DB, table string, data []map[string]interface{}) error {
