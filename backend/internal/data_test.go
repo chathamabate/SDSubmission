a51c647200ea39ba4c0ccd3543e9d7272dcbf4d8
chathamabate <chathamabate@gmail.com>
2024-01-05 11:11:27 -0500

    Unique Keys Testing

--- backend/internal/data_test.go
+++ backend/internal/data_test.go
@@ -76,7 +76,7 @@ type TestUniqueKeysCase struct {
 
 var TestUniqueKeysCases = []TestUniqueKeysCase {
     {
-        name: "Case 1",
+        name: "Simple Case 1",
         objs: []map[string]interface{}{
             {"Hello": 1},
             {"Goodbye": 1},
@@ -85,12 +85,52 @@ var TestUniqueKeysCases = []TestUniqueKeysCase {
             "Hello", "Goodbye",
         },
     },
+    {
+        name: "Simple Case 2",
+        objs: []map[string]interface{}{
+            {"Hello": 1, "Goodbye": 1},
+            {"Goodbye": 1},
+        },
+        expUks: []string{
+            "Hello", "Goodbye",
+        },
+    },
+    {
+        name: "Simple Case 3",
+        objs: []map[string]interface{}{
+            {"A": 1, "B": 1},
+            {"C": 1, "B": 1},
+            {"D": 1, "B": 1},
+        },
+        expUks: []string{
+            "A", "B", "C", "D",
+        },
+    },
+    {
+        name: "Simple Case 4",
+        objs: []map[string]interface{}{
+            {"A": 1, "B": 1, "C": 1},
+        },
+        expUks: []string{
+            "A", "B", "C",
+        },
+    },
 }
 
 func TestUniqueKeys(t *testing.T) {
-    /*
+    failure := false
     for _, uksCase := range TestUniqueKeysCases {
         actualUks := uniqueKeys(uksCase.objs)
+        
+        if !equalStringCollections(actualUks, uksCase.expUks) {
+            failure = true
 
-    }*/
+            t.Logf("Failure @ %s, actual: %v", 
+                uksCase.name, actualUks)
+        }
+    }
+
+    if failure {
+        t.Fail()
+    }
 }
