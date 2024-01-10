2ff7d3f3ef599256e048d7557ee5127d29cf6db5
chathamabate <chathamabate@gmail.com>
2024-01-10 13:36:36 -0500

    JSON Logging

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
+	"log/slog"
 	"os"
 
 	"github.com/chathamabate/SDSubmission/backend/internal"
@@ -10,18 +11,55 @@ import (
 
 
 func main() {
-    // Very simple logging strategy here.
-    f, err := os.OpenFile("sd.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
-    if err != nil {
-        fmt.Fprintln(os.Stderr, "Error openning log file.")
+    lfStr := flag.String("lf", "sd.log", "Log File")
+    dbfStr := flag.String("dbf", "DB.db", "Database File")
+    llStr := flag.String("ll", "INFO", "Log Level (DEBUG, INFO, WARN, ERROR)")
+
+    flag.Parse()
+
+    if len(*lfStr) == 0 {
+        fmt.Fprintln(os.Stderr, "Empty log filename given")
         os.Exit(1)
     }
-    log.SetOutput(f)
 
-    err = internal.RunSQLiteServer("DB.db")    
+    if len(*dbfStr) == 0 {
+        fmt.Fprintln(os.Stderr, "Empty db filename given")
+        os.Exit(1)
+    }
+
+    var ll slog.Level
+    switch (*llStr) {
+    case "DEBUG":
+        ll = slog.LevelDebug
+    case "INFO":
+        ll = slog.LevelInfo
+    case "WARN":
+        ll = slog.LevelWarn
+    case "ERROR":
+        ll = slog.LevelError
+    default:
+        fmt.Fprintf(os.Stderr, "Unknown log level given: %s\n", *llStr)
+        os.Exit(1)
+    }
+
+
+    // Very simple logging strategy here.
+    f, err := os.OpenFile(*lfStr, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+    if err != nil {
+        fmt.Fprintln(os.Stderr, "Error openning log file")
+        os.Exit(1)
+    }
+
+    jsonHandler := slog.NewJSONHandler(f, &slog.HandlerOptions{Level: ll})
+    jsonLogger := slog.New(jsonHandler)
+
+    slog.SetDefault(jsonLogger)
+
+    // Entery log errors in here...
+    err = internal.RunSQLiteServer(*dbfStr)    
 
     if err != nil {
-        log.Println(err)
+        slog.Error(err.Error())
     }
 }
 
